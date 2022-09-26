d3780ec5b93c4ee6d5c2e1c1e02ea8cf2b28fc00
Orifjon <orifjon.olimjonov@mail.ru>
2022-09-27 00:29:34 +0500

    add a broadcast

---
+++ app/src/main/java/uz/orifjon/todoappmvc/broadcast/AlarmManager.kt
@@ -0,0 +1,12 @@
+package uz.orifjon.todoappmvc.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmManager : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
