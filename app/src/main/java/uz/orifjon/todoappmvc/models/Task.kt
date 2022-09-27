9931ad247c919fdebc8c7d85ddfbed9a62c1380f
Orifjon <orifjon.olimjonov@mail.ru>
2022-09-27 22:54:05 +0500

    Room configure

--- app/src/main/java/uz/orifjon/todoappmvc/models/Task.kt
+++ app/src/main/java/uz/orifjon/todoappmvc/models/Task.kt
@@ -1,4 +1,17 @@
 package uz.orifjon.todoappmvc.models
 
-class Task {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    @ColumnInfo(name = "task_description")
+    val taskDescription:String,
+    @ColumnInfo(name = "task_time")
+    val taskTime:String,
+    @ColumnInfo(name = "task_category")
+    val taskCategory:String
+)
---
+++ app/src/main/java/uz/orifjon/todoappmvc/models/TaskDao.kt
@@ -0,0 +1,21 @@
+package uz.orifjon.todoappmvc.models
+
+import androidx.room.*
+
+@Dao
+interface TaskDao {
+
+
+    @Insert
+    fun add(task: Task)
+
+    @Update
+    fun update(task: Task)
+
+    @Delete
+    fun delete(task: Task)
+
+    @Query("SELECT * FROM task")
+    suspend fun list():List<Task>
+
+}
