61b159808a2f6ee02a0b38db5f56770ff7053265
Orifjon <orifjon.olimjonov@mail.ru>
2022-09-27 10:36:54 +0500

    initial commit

---
+++ app/src/main/cpp/todoappmvc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <jni.h>
+
+using namespace std;
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_uz_orifjon_todoappmvc_fragments_MainFragment_timePeriod(
+        JNIEnv *env,
+        jobject /* this */) {
+
+
+
+
+}
